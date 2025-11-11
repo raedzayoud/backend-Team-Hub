e9b1d3f69a6652f2543f2181939ff4a63f842917
raedzayoud <raed.zayoud.23@gmail.com>
2025-11-11 21:01:29 +0100

    fix some issue in part of configuration

--- src/main/java/com/smartjira/smartjira/config/SecurityConfig.java
+++ src/main/java/com/smartjira/smartjira/config/SecurityConfig.java
@@ -21,13 +21,13 @@ public class SecurityConfig {
                 .exceptionHandling(e -> e.authenticationEntryPoint(authenticationEntryPoint))
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/api/v1/task/**").authenticated()
-                        .requestMatchers("/api/v1/project/").authenticated()
-                        .requestMatchers("/api/v1/manager/").authenticated()
-                        .requestMatchers("/api/v1/task/").authenticated()
-                        .requestMatchers("/api/v1/developer/").authenticated()
-                        .requestMatchers("/api/v1/hr").authenticated()
-                        .requestMatchers("/api/v1/leave/").authenticated()
-                        .requestMatchers("/api/v1/salary/").authenticated()
+                        .requestMatchers("/api/v1/project/**").authenticated()
+                        .requestMatchers("/api/v1/manager/**").authenticated()
+                        .requestMatchers("/api/v1/task/**").authenticated()
+                        .requestMatchers("/api/v1/developer/**").authenticated()
+                        .requestMatchers("/api/v1/hr/**").authenticated()
+                        .requestMatchers("/api/v1/leave/**").authenticated()
+                        .requestMatchers("/api/v1/salary/**").authenticated()
                         .anyRequest().permitAll() // <-- all other endpoints are public
                 )
                 .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class);
